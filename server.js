1b8aeda3a27a43c87e2dfe5bb159a91918b26db6
aimontano <aimorales.montano@gmail.com>
2018-10-27 17:12:46 -0400

    Basic view

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/app/public/', express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/app/public/home.html'));
-});
+app.use(express.static('app/public'));
+
+// require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, () => console.log('server is on!'));
