972ccae7e35f37f1ca99f7b5b5c62e7ad175bdb9
aimontano <aimorales.montano@gmail.com>
2018-10-27 16:58:33 -0400

    Added some data to friends array

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+let friends = [
+	{
+		name: 'Potato Man',
+		image: 'http://nowiknow.com/wp-content/uploads/2012/05/mr-potato-head.jpg',
+		scores: [
+			4,
+			1,
+			2,
+			1,
+			4,
+			1,
+			4,
+			2,
+			4,
+			3
+		]
+	},
+	{
+		name: 'Spongebob',
+		image: 'https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg',
+		scores: [
+			1,
+		  4,
+		  5,
+		  2,
+		  4,
+		  4,
+		  1,
+		  5,
+		  5,
+		  2
+		]
+	}
+];
